4c653baf756ff1b47b328e5218ab60fe8441e41c
Ali Mert Kaya <mert_k4433@hotmail.com>
2024-07-16 21:35:09 +0300

    StudentExample updated

---
+++ StudentExample/src/PhdStudent.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+
+public class PhdStudent extends GraduateStudent {
+    private boolean qualifyingExamTaken;
+
+    public PhdStudent(int no, String name, int year, LocalDate dob, String major, String advisor, String thesis, boolean qualifyingExamTaken) {
+        super(no, name, year, dob, major, advisor, thesis);
+        this.qualifyingExamTaken = qualifyingExamTaken;
+    }
+    @Override
+    public void study() {
+        super.study();
+    }
+
+    @Override
+    public void register() {
+        super.register();
+    }
+
+    @Override
+    public void writeThesis() {
+        super.writeThesis();
+    }
+
+    @Override
+    public void meetWithAdvisor() {
+        super.meetWithAdvisor();
+    }
+
+    public void writePaper(String paperTitle) {
+        System.out.printf("PhdStudent is writing a paper titled: %s\n", paperTitle);
+    }
+}
